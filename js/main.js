3c4ca1855ada87d29ab4796a63138666b4430782
justincalderon <justin.calderon@outlook.com>
2017-04-13 21:34:51 -0700

    Clock HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,55 @@
-var tellTime = new Date()
 
-document.getElementById("button").addEventListener("click", function() {
-  document.getElementById("time").innerHTML = tellTime;
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Dependecies
+//
+// // truncateText method in 1 js file
+// function truncateText(element){
+//     if(element.innerHTML.length > 30){
+//         return element.innerHTML.substring(0,30) ;
+//     } else {
+//         return element ;
+//     }
+// }
+//
+// // addTwoNumbers method in 1 js file
+// function addTwoNumbers(a,b){
+//     return a + b ;
+// }
+//
+// // IIFE Immediately Invoked Function Expression
+// (function(truncate,add){
+//     // do something with those methods
+// }(truncateText(),addTwoNumbers()))
